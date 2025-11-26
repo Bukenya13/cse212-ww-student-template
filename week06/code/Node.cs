b5f83efa53529734a158e4db45f581da52f02fcc
Bukenya13 <phem.hancho@gmail.com>
2025-11-26 10:56:07 +0300

    pppiiqqw

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
 public class Node
 {
     public int Data { get; set; }
@@ -9,37 +13,78 @@ public class Node
         this.Data = data;
     }
 
+    // Problem 1: Insert Unique Values Only
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data) // Only insert if value is greater
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
+        // If value == Data, do nothing (prevents duplicates)
     }
 
+    // Problem 2: Contains
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
+    // Problem 4: GetHeight
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
+    }
+
+    // Problem 3: Traverse Backwards
+    public IEnumerable<int> TraverseBackward()
+    {
+        if (Right != null)
+        {
+            foreach (var value in Right.TraverseBackward())
+                yield return value;
+        }
+
+        yield return Data;
+
+        if (Left != null)
+        {
+            foreach (var value in Left.TraverseBackward())
+                yield return value;
+        }
+    }
+
+    // Optional helper: Traverse Forward (for reference)
+    public IEnumerable<int> TraverseForward()
+    {
+        if (Left != null)
+        {
+            foreach (var value in Left.TraverseForward())
+                yield return value;
+        }
+
+        yield return Data;
+
+        if (Right != null)
+        {
+            foreach (var value in Right.TraverseForward())
+                yield return value;
+        }
     }
 }
